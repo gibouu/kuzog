c1633b4bbe9c70ccb99fdd69d3033023bd6525f5
gibouu <gibril@bachouchi.com>
2026-06-06 20:47:38 +0200

    chore: delete NavigationContext stub (no consumers remain)
    Refs #13
    
    — Claude + gib

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './contexts/LanguageContext';
-import { NavigationProvider } from './contexts/NavigationContext';
 import App from './App';
 import BlogPage from './pages/BlogPage';
 import HydrobioPage from './pages/HydrobioPage';
@@ -19,17 +18,15 @@ if (!rootElement) {
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <LanguageProvider>
-      <NavigationProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/hydrobio" element={<HydrobioPage />} />
-            <Route path="/microplantes" element={<MicroplantesPage />} />
-            <Route path="/group" element={<GroupPage />} />
-            <Route path="/blog" element={<BlogPage />} />
-          </Routes>
-        </BrowserRouter>
-      </NavigationProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/hydrobio" element={<HydrobioPage />} />
+          <Route path="/microplantes" element={<MicroplantesPage />} />
+          <Route path="/group" element={<GroupPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+        </Routes>
+      </BrowserRouter>
     </LanguageProvider>
   </React.StrictMode>
 );
